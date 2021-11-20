b3965fbca93e72390464b436b4274b8c814a01e9
Sidprogrammer <sidprogramrelated@gmail.com>
2021-11-20 16:46:43 +0530

    Update LayoutEffectTutorial.js

--- src/UseLayoutEffect/LayoutEffectTutorial.js
+++ src/UseLayoutEffect/LayoutEffectTutorial.js
@@ -3,6 +3,7 @@ import { useLayoutEffect, useEffect, useRef } from "react";
 function LayoutEffectTutorial() {
   const inputRef = useRef(null);
 
+  /////used to perfrom some operation on layout before the page is rendered
   useLayoutEffect(() => {
     console.log(inputRef.current.value);
   }, []);
